b23933986788457231a6921b547487308b503ebb
Jasper Cuvelier <webmaster@sgmeulebekepittem.be>
2018-08-07 17:54:09 +0200

    Added floating for navbar, added feature: parameter page in url loads the correct article in the article parameter in the main index file.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ $(".navToggle").on('click',function(){
   });
 
 });
+
+
+function loadArticle() {
+  console.log(window.location.href);
+  let url = window.location.href;
+  let requestedPage = url.split("=")[1];
+  console.log(requestedPage);
+  $("article").load("articles/"+requestedPage+"/index.html",function(response,status,xhr){
+    if ( status == "error" )
+    {
+    var msg = "Sorry but there was an error: ";
+    console.log( msg + xhr.status + " " + xhr.statusText );
+     $("main").css("height",'75%');
+    }
+    else {
+
+  //
+    }
+  });
+
+}
